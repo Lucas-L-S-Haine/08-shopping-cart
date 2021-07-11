dec660a678b1e19173cac7173748a8b073632711
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-07-11 07:11:21 -0300

    Linhas de código de warnings comentadas

--- script.js
+++ script.js
@@ -26,21 +26,21 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 async function getProducts() {
   const response = await fetch(`${baseUrl}/search?q=computador`);
@@ -48,18 +48,19 @@ async function getProducts() {
   return product;
 }
 
-function addProductsToScreen(product) {
-
-}
+// function addProductsToScreen(product) {
+//
+// }
 
 window.onload = async () => {
   const products = await getProducts();
   console.log(products.results);
   products.results.forEach((product) => {
     const element = createProductItemElement(product);
-    console.log(element);
-
     const items = document.querySelector('.items');
+    const addButton = document.createElement('button');
+    element.children[3].innerHTML = addButton;
     items.appendChild(element);
+    console.log(element.children);
   });
 };
